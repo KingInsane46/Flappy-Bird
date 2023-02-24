7f8501adf8cd33df7f0786e74ddae4720e82458a
Insane <81003264+KingInsane46@users.noreply.github.com>
2023-02-24 15:22:21 -0600

    Add files via upload

---
+++ Flappy Bird/src/Main.java
@@ -0,0 +1,18 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        JFrame frame = new JFrame("Flappy Bird");
+        frame.setSize(500, 750);
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+    }
+}
