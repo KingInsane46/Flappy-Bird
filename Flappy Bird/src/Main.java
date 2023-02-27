aeae307b2ce48b82fe0cd801e9f90a3088fb82f1
Insane <81003264+KingInsane46@users.noreply.github.com>
2023-02-27 10:08:04 -0600

    Add files via upload

--- Flappy Bird/src/Main.java
+++ Flappy Bird/src/Main.java
@@ -4,15 +4,25 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+public class Main
+{
+    public static void main(String[] args) throws IOException
+    {
+        File png1 = new File("flappy-bird-assets/sprites/1.png");
+
+        JLabel lab0 = new JLabel();
+        lab0.setIcon(new ImageIcon(ImageIO.read(new File("flappy-bird-assets/sprites/0.png"))));
+
         JFrame frame = new JFrame("Flappy Bird");
         frame.setSize(500, 750);
         frame.setResizable(false);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setVisible(true);
 
+        frame.add(lab0);
+
+        frame.setVisible(true);
     }
 }
