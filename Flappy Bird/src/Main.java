a79b736d12696a630239bef9b2cab1f96bf52397
Insane <81003264+KingInsane46@users.noreply.github.com>
2023-03-06 10:09:31 -0600

    Add files via upload

--- Flappy Bird/src/Main.java
+++ Flappy Bird/src/Main.java
@@ -8,16 +8,16 @@ import java.io.IOException;
 import java.time.Clock;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 class Main
 {
-    static List<GameObject> objectsToDraw = new ArrayList<GameObject>();
-    public static void main(String[] args) throws InterruptedException, IOException {
+    static List<GameObject> objectsToDraw = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
         BufferedImage backgroundImage = ImageIO.read(new File("flappy-bird-assets/sprites/background-day.png"));
         BufferedImage pipeImage = ImageIO.read(new File("flappy-bird-assets/sprites/pipe-green.png"));
-
         BufferedImage playerDown = ImageIO.read(new File("flappy-bird-assets/sprites/yellowbird-downflap.png"));
+        BufferedImage playerMid = ImageIO.read(new File("flappy-bird-assets/sprites/yellowbird-midflap.png"));
+        BufferedImage playerUp = ImageIO.read(new File("flappy-bird-assets/sprites/yellowbird-upflap.png"));
 
         GameObject background = new GameObject("Background");
         background.setDrawImage(backgroundImage);
@@ -28,16 +28,21 @@ class Main
         GameObject pipePrefab = new GameObject("Pipe Prefab");
         pipeTop.setDrawImage(pipeImage);
         pipeBottom.setDrawImage(pipeImage);
-        pipeTop.setVisible(false);
-        pipeBottom.setVisible(false);
         pipeTop.setTransform(0, -200, 180);
         pipeBottom.setTransform(0, 200, 0);
         pipePrefab.addChild(pipeTop);
         pipePrefab.addChild(pipeBottom);
         pipePrefab.setPosition((double) 288/2, (double) 512/2);
+        pipePrefab.setAllVisible(false);
 
         GameObject pipe0 = pipePrefab.createCopy();
         pipe0.setAllVisible(true);
+
+        /*GameObject player = new GameObject("Player", 1);
+        player.addAnimation(playerDown);
+        player.addAnimation(playerMid);
+        player.addAnimation(playerUp);
+        player.setPosition((double) 288/2, (double) 512/2);*/
 
         long currentTime = Clock.systemUTC().millis(), previousTime = currentTime;
         double deltaTime;
@@ -75,17 +80,28 @@ class Main
 
 class GameObject {
     private final List<GameObject> children = new ArrayList<>();
+    private final List<BufferedImage> animations = new ArrayList<>();
     private BufferedImage drawImage = null;
     private String name;
-    private double x = 0, y = 0, rotation = 0;
-    private int width = 0, height = 0;
-    private boolean visible = true;
+    private double x = 0, y = 0, rotation = 0, animationSpeed = 0, nextAnimationChangeTime = Clock.systemUTC().millis();
+    private int width = 0, height = 0, currentAnimationFrame = 0;
+    private boolean visible = true, animated = false;
 
     public GameObject(String name)
     {
         Main.objectsToDraw.add(this);
         setName(name);
     }
+    public GameObject(String name, double animationSpeed)
+    {
+        Main.objectsToDraw.add(this);
+        setName(name);
+        this.animationSpeed = animationSpeed;
+        animated = true;
+    }
+
+
+    //Transform
     public void setTransform(double x, double y, double rotation)
     {
         setPosition(x, y);
@@ -126,6 +142,9 @@ class GameObject {
     {
         return rotation;
     }
+
+
+    //Name
     public void setName(String name)
     {
         this.name = name;
@@ -134,6 +153,9 @@ class GameObject {
     {
         return name;
     }
+
+
+    //Children
     public void addChild(GameObject child)
     {
         if(children.contains(child))
@@ -159,16 +181,9 @@ class GameObject {
                 return child;
         return null;
     }
-    public void setDrawImage(BufferedImage image)
-    {
-        this.drawImage = image;
-        width = image == null ? 0 : image.getWidth();
-        height = image == null ? 0 : image.getHeight();
-    }
-    public BufferedImage getDrawImage()
-    {
-        return drawImage;
-    }
+
+
+    //Visibility
     public void setVisible(boolean value)
     {
         visible = value;
@@ -183,10 +198,58 @@ class GameObject {
     {
         return visible;
     }
+
+
+    //Animations
+    public void setAnimationSpeed(double speed)
+    {
+        animationSpeed = speed;
+        animated = speed > 0;
+    }
+    public double getAnimationSpeed()
+    {
+        return animationSpeed;
+    }
+    public void addAnimation(BufferedImage image)
+    {
+        animations.add(image);
+        animated = true;
+    }
+    public void removeAnimation(BufferedImage image)
+    {
+        animations.remove(image);
+        animated = animations.size() > 0;
+    }
+    public BufferedImage getCurrentFrame()
+    {
+        return animations.get(currentAnimationFrame);
+    }
+    public void setDrawImage(BufferedImage image)
+    {
+        this.drawImage = image;
+        width = image == null ? 0 : image.getWidth();
+        height = image == null ? 0 : image.getHeight();
+    }
+    public BufferedImage getDrawImage()
+    {
+        return drawImage;
+    }
+
+
+    //Utils
     public void draw(Graphics2D g)
     {
         for(GameObject child : children)
             child.draw(g);
+
+        if(animations && Clock.systemUTC().millis() > nextAnimationChangeTime)
+        {
+            nextAnimationChangeTime = Clock.systemUTC().millis() + animationSpeed;
+            currentAnimationFrame++;
+            if(currentAnimationFrame > animations.size())
+                currentAnimationFrame = 0;
+            setDrawImage(animations.get(currentAnimationFrame));
+        }
         AffineTransform originalTransform = g.getTransform();
         g.translate(x, y);
         g.rotate(Math.toRadians(rotation));
@@ -196,10 +259,10 @@ class GameObject {
     }
     public GameObject createCopy()
     {
-        GameObject copy = new GameObject(getName());
-        copy.setTransform(getX(), getY(), getRotation());
-        copy.setDrawImage(getDrawImage());
-        copy.setVisible(getVisible());
+        GameObject copy = new GameObject(name, animationSpeed);
+        copy.setTransform(x, y, rotation);
+        copy.setDrawImage(drawImage);
+        copy.setVisible(visible);
         for(GameObject child : children)
             copy.addChild(child.createCopy());
         return copy;
